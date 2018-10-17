da52fc150860afb77e0f18cf97ec6d325b060154
Chris Elliott <biol75@york.ac.uk>
2018-10-17 16:30:05 +0100

    cut video to flash and 1s

--- video_analysis/Per_Video_movement_trace.py
+++ video_analysis/Per_Video_movement_trace.py
@@ -39,27 +39,39 @@ cap = cv2.VideoCapture(fileName)
 fWriteName = fileName.replace ('.avi', '.csv')
 fWriteName = fWriteName.rsplit('/', 1)[-1]
 file = open(fWriteName,"w")
+file.write('index,' + 'mean_val,' + 'distance,' + 'perimeter' + "\n")
 
-ret, frame = cap.read()
+#global variables..
 i = 1
-# look at first image
+mean_val = 0
+perimeter = 0
+distance = 0
+seen_flash = False
 
+# look at first image
+ret, frame = cap.read()
+img = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)  # RGB file to greyscale
+init_mean_val = cv2.mean(img) [0]
 
 while (ret):
-    #find position/roi in first frame then follow in each frame
-
-
 #Video manipulation/editing
 #Converts video to greyscale, binary and blurs the image
-    img = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)  # RGB file to greyscale
-    ret, Binary = cv2.threshold(img, MyThreshold, 255, cv2.THRESH_BINARY) #Greyscale to binary, can adjust threshold by changing first number in brackets.
+    ret, Binary = cv2.threshold(img, MyThreshold, 255, cv2.THRESH_BINARY) #Greyscale to binary, can adjust threshold by changing MyThreshold.
     blurred = cv2.GaussianBlur(Binary, (7,7),  0)
     blurred = cv2.erode(blurred, None, iterations =2)
     blurred = cv2.dilate(blurred, None, iterations =2)
     
-    #calculate mean intensity of the greyscale image
-    mean_val = cv2.mean(img)
-
+#calculate mean intensity of the greyscale image and test if we have a flash
+    tmp_mean_val = cv2.mean(img) [0]
+    if tmp_mean_val > 4 * init_mean_val :
+        seen_flash = True
+        
+#print distance #Can also be altered to allow the data to be exported to an external file, instead of displayed in the running window.
+    if (seen_flash):
+        file.write(repr(i) +',' + repr(mean_val) + ',' + repr(distance) + ',' + repr(perimeter) + "\n")
+    
+    mean_val = tmp_mean_val    
+        
 ##Finding distance between centre of the eye and the tip of the proboscis
 #Finds contours of object in the video
     # was im, contours, hierarchy = cv2.findContours(blurred, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -67,7 +79,7 @@ while (ret):
     cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
     
 #   cv2.drawContours(frame, contours, -1, (0, 255, 0), 2) #adds contour lines to video
-    cv2.drawContours(frame, cnts, 0, (255, 255, 0), 2) #adds contour lines to video
+    cv2.drawContours(frame, cnts, 0, (0, 255, 0), 2) #adds contour lines to video
 
 # Determine the lowest point along the contour
     count = cnts[-1]
@@ -77,13 +89,11 @@ while (ret):
     list(zip(bottommost,eye))  
     distance = sqrt((bottommost[0] - eye[0])**2 + (bottommost[1]-eye[1])**2) #finds the distance between the coordinates    
 
-# length of contour    
-    perimeter = cv2.arcLength(count,True)
+# length of (smoothed) contour
+    epsilon = 0.05*cv2.arcLength(cnts[0], True)
+    approx = cv2.approxPolyDP(cnts[0], epsilon, True)    
+    perimeter = cv2.arcLength(approx,True)
     
-#print distance #Can also be altered to allow the data to be exported to an external file, instead of displaeyd in the running window.
-    file.write(repr(i) +',' + repr(mean_val[0]) + ',' + repr(distance) + ',' + repr(perimeter) + "\n")
-
-
 # Applies a circle to the lowest point of the proboscis and the centre of the eye so that when displayed the point being tracked can be observed.
     cv2.circle(frame, bottommost, 5, (255, 255, 0), -1)
     cv2.circle(frame, eye, 5, (255, 255, 0), -1)
@@ -92,13 +102,20 @@ while (ret):
     cv2.imshow('Original', frame)
     cv2.imshow('Binary', Binary)
 
+    
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
+        
     ret, frame = cap.read()
-    i = i + 1 # count the frames
+    img = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)  # RGB file to greyscale
     
+    if (seen_flash):
+        i = i + 1 # count the frames
     
+    if (i > 200):  # 200 frames is about 1s
+        break  # finish data
+        
 cap.release()
 cv2.destroyAllWindows()
 file.close()
