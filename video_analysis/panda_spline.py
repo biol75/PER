54233c8617e0b0598ed27d48b55f5016ab53fba5
Chris Elliott <biol75@york.ac.uk>
2018-10-23 13:01:38 +0100

    better smootthing

--- video_analysis/panda_spline.py
+++ video_analysis/panda_spline.py
@@ -4,6 +4,7 @@ import pandas
 import sys
 # use splines to fit and interpolate data
 from scipy.interpolate import interp1d
+from scipy.interpolate import UnivariateSpline
 from scipy.optimize import fmin
 import numpy as np
 import matplotlib.pyplot as plt
@@ -29,24 +30,24 @@ x=np.array(df.index)
 y=np.array(df.perimeter)
 
 # create the interpolating function
-f = interp1d(x, y, kind='cubic', bounds_error=False)
+#f = interp1d(x, y, kind='cubic', bounds_error=False)
+spl = UnivariateSpline(x, y, s=50000)
 
 # to find the maximum, we minimize the negative of the function. We
 # cannot just multiply f by -1, so we create a new function here.
-f2 = interp1d(x, -y, kind='cubic')
-xmax = fmin(f2, 150)
+# f2 = interp1d(x, -y, kind='cubic')
+# xmax = fmin(f2, 150)
 
 xfit = np.linspace(7,198)
 
 plt.plot(x,y,'bo')
-plt.plot(xfit, f(xfit),'r-')
-plt.plot(xmax, f(xmax),'g*')
-plt.legend(['data','fit','max'], loc='best', numpoints=1)
+plt.plot(xfit, spl(xfit),'r-')
+# plt.plot(xmax, f(xmax),'g*')
+# plt.legend(['data','fit','max'], loc='best', numpoints=1)
 plt.xlabel('x data')
 plt.ylabel('y data')
-plt.title('Max point = ({0:1.2f}, {1:1.2f})'.format(float(xmax),
-                                                    float(f(xmax))))
-                                                    
+# plt.title('Max point = ({0:1.2f}, {1:1.2f})'.format(float(xmax), float(f(xmax))))
+plt.show()                                                    
                                                     
 #pdb.set_trace()                                                                                                      
 figName = fileName.replace ('.csv', '.png')
