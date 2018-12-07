3decedc7417fda2b4a387bcbb30ab847f65ee107
Chris <cje2@york.ac.uk>
2018-12-07 11:07:28 +0000

    iterate current directory

--- video_analysis/align_graph.py
+++ video_analysis/align_graph.py
@@ -4,20 +4,27 @@ import os
 import pdb
 
 
-mat2=np.zeros((193,2000))
-filecount = 0
+
+
+
+rootdir = '.'
+
+
 myfileList = []
-
-rootdir = '../../../3Dec/DicerGr5aOPRR10tripTHG'
-
-
+# make list of files
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         #print os.path.join(subdir, file)
         filepath = subdir + os.sep + file
-        pdb.set_trace()
-
+        
         if filepath.endswith(".csv"):
+            if file.startswith("fc"):
+                myfileList.append (filepath)
+            
+
+mat2=np.zeros((193,len(myfileList)))
+filecount = 0
+for filepath in myfileList:
             print (filepath)
             #pdb.set_trace()
             df = pandas.read_csv(filepath, header=0, 
@@ -25,15 +32,14 @@ for subdir, dirs, files in os.walk(rootdir):
             df1 = df[['actual area max']]
             
             x = df[['actual area max']].iloc[2:195,[0]]
+            
             mat2 [:,[filecount]] = x
-            
-            
-            myfileList.append (filepath)
+                                  
             filecount = filecount + 1
             
 pdb.set_trace()
 
-mat2=np.delete(mat2, slice(filecount,2000), 1) # 1 means delete columns
+#mat2=np.delete(mat2, slice(filecount,2000), 1) # 1 means delete columns
 out_df = pandas.DataFrame(data = mat2)
 out_df.columns=myfileList
 out_df.to_csv('out.csv')
