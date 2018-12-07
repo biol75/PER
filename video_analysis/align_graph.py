b97724f657ad743afe705797e59900c8530854aa
Chris <cje2@york.ac.uk>
2018-12-07 10:26:59 +0000

    csv generated from one directory

--- video_analysis/align_graph.py
+++ video_analysis/align_graph.py
@@ -15,6 +15,7 @@ for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         #print os.path.join(subdir, file)
         filepath = subdir + os.sep + file
+        pdb.set_trace()
 
         if filepath.endswith(".csv"):
             print (filepath)
